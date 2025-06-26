71f017d6c116864beff7c783d1946a0da6b9d6d0
arraviayuuu <arraviayu@gmail.com>
2025-06-26 20:29:25 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navMenu = document.querySelector(".nav-menu");
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+  });
+
   function tampilkanRumusDanHasil(containerId, rumusHTML, hasilText) {
     const container = document.getElementById(containerId);
     container.innerHTML = `
